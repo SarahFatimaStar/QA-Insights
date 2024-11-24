445c8d581da49756d7207f24b1ff1f0bb4270212
SarahAbbasGit <sarahfatimastar1992@gmail.com>
2024-11-24 11:54:32 +0500

    Create class Main.java

---
+++ Java Practice/class Main.java
@@ -0,0 +1,16 @@
+class Main
+{
+public static void main (String args [])
+{
+int x = 5;
+int y = 10;
+int temp;
+
+System.out.println("before swap, x ="+x+" and y ="+y);
+temp = x;
+x = y ;
+y = temp;
+
+System.out.println("after swap, x ="+x+" and y ="+y);
+}
+}
